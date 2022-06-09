b2f9d258eba0bfcf562a816fc60f4cf83061d07f
afanasii-siladi <afanasijsiladi@gmail.com>
2022-06-09 11:40:00 +0200

    создал новую папку где храница дикт импортировал і  принтанул в главний файл

---
+++ constants.py
@@ -0,0 +1,12 @@
+d = [{'product_name':'french fries', 'price':45},
+{'product_name':'burg', 'price':135},
+{'product_name':'pizza', 'price':95},
+{'product_name':'cola', 'price':29},
+{'product_name':'fanta', 'price':39},
+{'product_name':'sprite', 'price':39},
+{'product_name':'apple juice', 'price':20},
+{'product_name':'cherry juice', 'price':20},
+{'product_name':'multifruit juice', 'price':20},
+{'product_name':'yogurt', 'price':45},
+{'product_name':'cake', 'price':45},
+{'product_name':'ice cream', 'price':15}]
