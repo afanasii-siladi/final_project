0221abe263339f22c37cf7d53e9f6304014a0dbd
afanasii-siladi <afanasijsiladi@gmail.com>
2022-06-25 18:25:21 +0200

    змінив тест з російської мови на  українську, записав декілька частин коду в функцію та змінив запиис в файл тепер не append_ а write та назва містить в собі назву користовача якого записується в файлда додав файл куди записав які зміни ще потрібно зробити в кодію щоб не забути и остане додав readme.md  файл який з часом буду дописувати

--- check.py
+++ check.py
@@ -1,20 +1,48 @@
-'''  
-if order_user.lower() == 'картошка фри':
-#        q1 += 45
-elif order_user.lower() == 'бургер':
-        q1 += 135
-elif order_user.lower() == 'пицца':
-        q1 += 95
-elif order_user.lower() in ('спрайт', 'фанта', 'кола'):
-        q1 += 39       
-elif order_user.lower() in ('яблочный сок', 'вишньовый сок', 'сок мултифрукт'):
-        q1 += 20
-elif order_user.lower() == 'йогурт':
-        q1 += 45
-elif order_user.lower() == 'тортик':
-        q1 += 55
-elif order_user.lower() == 'морожено':
-        q1 += 15
-for pruduct in d:
-        print(f'{} - {[]}')
-'''          
+from constants import dict
+delivery_cost = 50
+
+menu = [product['product_name'] for product in dict]
+
+user_name = input('Введите свое имя-> ')
+print('Наше меню:')
+for product in dict:
+        print(f"{product['product_name']} - {product['price']}")
+
+while True:
+        order_user = input('Введите свой заказ из меню -> ')
+        if order_user.lower() not in menu:
+                print('Вы не правельно записали заказ. Можете ввести заного свой заказ', user_name )                
+        else:
+                print(order_user.lower())   
+                delivery_cost += [product2['price'] for product2 in dict if product2['product_name'] == order_user.lower()][0]
+                              
+                print('Вам нужно заплатить за заказ -> ',delivery_cost)
+                pay = input('Вам хватает средств для оплаты(да или нет) -> ')
+
+                if pay.lower() not in ('да', 'нет'):
+                        print('Вы должны ввести да или нет иначе у нас не выйдет доставить заказ', user_name,)
+                else:
+                        def with_write_file():
+                                with open('Order/order.txt', 'a', encoding='utf-8') as f:
+                                        f.write(order_user) 
+
+                        if pay.lower() == 'да':
+                                print('Спасибо за покупку заказ будет скоро у вас ', user_name, '.')  
+                                with_write_file()
+                                break
+                                а = input('Вы хотите изменить или отменить свой заказ -> ')
+                                print(order_user)
+                                if а.lower() not in ('отменить', 'изменить'):
+                                        print('Такой ответ не возможен!')
+                                else:
+
+                                        if а.lower() == 'изменить':
+                                                order_user = input('Введите свой заказ из меню -> ')
+                                                if order_user not in menu:
+                                                        print('Error')
+                                                print(order_user)
+                                                with_write_file()     
+                                        else:
+                                                print('Спасибо что посетили наш сайт')       
+                                                with_write_file()      
+                            
--- main.py
+++ main.py
@@ -1,47 +1,36 @@
 from constants import dict
 delivery_cost = 50
 
-menu = [ product['product_name'] for product in dict]
+menu = [product['product_name'] for product in dict]
 
-user_name = input('Введите свое имя-> ')
-print('Наше меню:')
-for product in dict:
+user_name = input('Запишіть своє ім"я -> ')
+def menu():
+    print('Наше меню:')
+    for product in dict:
         print(f"{product['product_name']} - {product['price']}")
 
-while True:
-        order_user = input('Введите свой заказ из меню -> ')
+menu()
+
+def main():       
+                     
+    while True:
+        order_user = input('запишіть замовлення з меню -> ')
         if order_user.lower() not in menu:
-                print('Вы не правельно записали заказ. Можете ввести заного свой заказ', user_name )                
+                print('Виправіть своє  замовлення, буд ласка. Можете записати по-новому своє замовлення')                
         else:
+            menu()
+            add_oder = input('Якщо потрібно доповнити замовлення продовжуйте свій список далі, якщо це все напишіть замовлення готово -> ')
+            if add_oder.lower() == 'замовлення готово':
+                print('Вартість замовлення -> ',delivery_cost)
+                print('Дякую за покупку замовлення буде скоро у вас.')  
+                def with_write_file():
+                    with open('Order/order_for_{user_name}.txt', 'w', encoding='utf-8') as f:
+                            f.write(order_user)
+                with_write_file()
+                break
+            elif order_user.lower() not in menu:
+                print('Виправіть своє  замовлення, буд ласка. Можете записати по-новому своє замовлення')            
+            else:
                 print(order_user.lower())   
                 delivery_cost += [product2['price'] for product2 in dict if product2['product_name'] == order_user.lower()][0]
-                              
-                print('Вам нужно заплатить за заказ -> ',delivery_cost)
-                pay = input('Вам хватает средств для оплаты(да или нет) -> ')
-
-                if pay.lower() not in ('да', 'нет'):
-                        print('Вы должны ввести да или нет иначе у нас не выйдет доставить заказ', user_name,)
-                else:
-                        def with_write_file():
-                                with open('Order/order.txt', 'a', encoding='utf-8') as f:
-                                        f.write(order_user) 
-
-                        if pay.lower() == 'да':
-                                print('Спасибо за покупку заказ будет скоро у вас ', user_name, '.')  
-                                with_write_file()
-                                break
-                                а = input('Вы хотите изменить или отменить свой заказ -> ')
-                                print(order_user)
-                                if а.lower() not in ('отменить', 'изменить'):
-                                        print('Такой ответ не возможен!')
-                                else:
-
-                                        if а.lower() == 'изменить':
-                                                order_user = input('Введите свой заказ из меню -> ')
-                                                if order_user not in menu:
-                                                        print('Error')
-                                                print(order_user)
-                                                with_write_file()     
-                                        else:
-                                                print('Спасибо что посетили наш сайт')       
-                                                with_write_file()
+                print('Дкую що використовували наш сайт.')                                                 
