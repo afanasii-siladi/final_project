16eb24be38dec569c8d2c767d7fc8bd6b2b60fb9
afanasii-siladi <afanasijsiladi@gmail.com>
2022-06-26 13:42:06 +0200

    переписав назви продуктів на ураїнську мову

--- constants.py
+++ constants.py
@@ -1,12 +1,12 @@
-dict = [{'product_name':'картошка фри', 'price':45},
+dict = [{'product_name':'картопля фрі', 'price':45},
 {'product_name':'бургер', 'price':135},
-{'product_name':'пицца', 'price':95},
+{'product_name':'піцца', 'price':95},
 {'product_name':'кола', 'price':39},
 {'product_name':'спрайт', 'price':39},
 {'product_name':'фанта', 'price':39},
-{'product_name':'яблочный сок', 'price':20},
-{'product_name':'вишньовый сок', 'price':20},
-{'product_name':'сок мултифрукт', 'price':20},
+{'product_name':'яблуневий сік', 'price':20},
+{'product_name':'вишневий сік', 'price':20},
+{'product_name':'сік мултифрукт', 'price':20},
 {'product_name':'йогурт', 'price':45},
 {'product_name':'тортик', 'price':45},
-{'product_name':'морожено', 'price':15}] 
+{'product_name':'морозиво', 'price':15}] 
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ delivery_cost = 50
 
 menu = [product['product_name'] for product in dict]
 
-user_name = input('Запишіть своє ім"я -> ')
+user_name = input("Запишіть своє ім'я -> ")
 def menu():
     print('Наше меню:')
     for product in dict:
@@ -11,16 +11,15 @@ def menu():
 
 menu()
 
-def main():       
-                     
+def main():                          
     while True:
         order_user = input('запишіть замовлення з меню -> ')
         if order_user.lower() not in menu:
                 print('Виправіть своє  замовлення, буд ласка. Можете записати по-новому своє замовлення')                
         else:
-            menu()
             add_oder = input('Якщо потрібно доповнити замовлення продовжуйте свій список далі, якщо це все напишіть замовлення готово -> ')
             if add_oder.lower() == 'замовлення готово':
+                delivery_cost += [product2['price'] for product2 in dict if product2['product_name'] == order_user.lower()][0]
                 print('Вартість замовлення -> ',delivery_cost)
                 print('Дякую за покупку замовлення буде скоро у вас.')  
                 def with_write_file():
@@ -33,4 +32,7 @@ def main():
             else:
                 print(order_user.lower())   
                 delivery_cost += [product2['price'] for product2 in dict if product2['product_name'] == order_user.lower()][0]
-                print('Дкую що використовували наш сайт.')                                                 
+                print('Вартість замовлення -> ',delivery_cost)
+                print('Дкую що використовували наш сайт.')
+                
+main()                                                                
