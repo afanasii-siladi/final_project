25c65d7cc0b0c88ad35254dd0628d54be36cae6c
afanasii-siladi <afanasijsiladi@gmail.com>
2022-06-27 16:51:59 +0200

    доробив всі нотатки з mistake.txt

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 from constants import dict
-delivery_cost = 50
+delivery_cost = 0
+delivery = 50
+delivery_cost += delivery
 
 menu = [product['product_name'] for product in dict]
 
+
 user_name = input("Запишіть своє ім'я -> ")
-def menu():
-    print('Наше меню:')
-    for product in dict:
-        print(f"{product['product_name']} - {product['price']}")
+print('Наше меню:')
+for product in dict:
+    print(f"{product['product_name']} - {product['price']}")
 
-menu()
 
-def main():                          
-    while True:
-        order_user = input('запишіть замовлення з меню -> ')
-        if order_user.lower() not in menu:
-                print('Виправіть своє  замовлення, буд ласка. Можете записати по-новому своє замовлення')                
-        else:
-            add_oder = input('Якщо потрібно доповнити замовлення продовжуйте свій список далі, якщо це все напишіть замовлення готово -> ')
-            if add_oder.lower() == 'замовлення готово':
-                delivery_cost += [product2['price'] for product2 in dict if product2['product_name'] == order_user.lower()][0]
-                print('Вартість замовлення -> ',delivery_cost)
-                print('Дякую за покупку замовлення буде скоро у вас.')  
-                def with_write_file():
-                    with open('Order/order_for_{user_name}.txt', 'w', encoding='utf-8') as f:
-                            f.write(order_user)
-                with_write_file()
-                break
-            elif order_user.lower() not in menu:
-                print('Виправіть своє  замовлення, буд ласка. Можете записати по-новому своє замовлення')            
+                        
+while True:
+    order_user = input('запишіть замовлення з меню -> ')
+    if order_user.lower() not in menu:
+        print('Виправіть своє  замовлення, буд ласка. Можете записати по-новому своє замовлення')                
+    else:
+        delivery_cost += [product2['price'] for product2 in dict if product2['product_name'] == order_user.lower()][0]
+        print('Вартість за доставку -> ',delivery)
+        print('Вартість замовлення -> ',delivery_cost)
+        not_add_oder = input('Якщо це весь сипос напишіть /замовлення готово, якщо потрібно продовжити замволення на друкуйте /далі -> ')
+        if not_add_oder.lower() == 'замовлення готово':
+            print('Дякую за покупку, незабаром замовлення прибуде до вас.')  
+            with open('Order/order_for_{user_name}.txt', 'w', encoding='utf-8') as f:
+                    f.write(order_user)
+                    f.write(';')
+                    print('None')
+            break
+        elif not_add_oder.lower() == 'далі':
+            add_oder = input('доповнити замовлення можливо на цьому кроці -> ')
+            if add_oder.lower() not in menu:
+                print('Виправіть своє  замовлення, буд ласка. Потрібно записати по-новому замовлення')            
             else:
                 print(order_user.lower())   
-                delivery_cost += [product2['price'] for product2 in dict if product2['product_name'] == order_user.lower()][0]
-                print('Вартість замовлення -> ',delivery_cost)
+                print(add_oder.lower())
+                delivery_cost1 = delivery_cost + [product2['price'] for product2 in dict if product2['product_name'] == add_oder.lower()][0] + [product2['price'] for product2 in dict if product2['product_name'] == order_user.lower()][0]
+                print('Вартість за доставку -> ',delivery)
+                print('Вартість замовлення -> ',delivery_cost1)
                 print('Дкую що використовували наш сайт.')
+                with open('Order/order_for_{user_name}.txt', 'w', encoding='utf-8') as f:
+                    f.write(order_user)
+                    f.write(';')
+                    f.write(add_oder)
+                    f.write(';')
+                    print('None')
+                break
                 
-main()                                                                
